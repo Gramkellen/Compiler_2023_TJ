fa6bacdb08c3e24ec25c2531fe76e23dae4ca5fe
He Yinghao <granthe342@outlook.com>
2023-12-24 21:46:12 +0800

    Antlr4

---
+++ antlr4_test/src/main/java/result/HelloBaseVisitor.java
@@ -0,0 +1,22 @@
+// Generated from C:/Works/����/����ԭ��/antlrs/antlr4_test/src/main/java/org/example/Hello.g4 by ANTLR 4.13.1
+package result;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link HelloVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class HelloBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements HelloVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitR(HelloParser.RContext ctx) { return visitChildren(ctx); }
+}
---
+++ antlr4_test/src/main/java/result/HelloVisitor.java
@@ -0,0 +1,19 @@
+// Generated from C:/Works/����/����ԭ��/antlrs/antlr4_test/src/main/java/org/example/Hello.g4 by ANTLR 4.13.1
+package result;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link HelloParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface HelloVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link HelloParser#r}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitR(HelloParser.RContext ctx);
+}
